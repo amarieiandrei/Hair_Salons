d8720de09be2b37011f0aaa54d2400a754ec5fd5
Andrei Amariei <amarieiandrei60@gmail.com>
2023-04-12 22:01:01 +0300

    login-form

--- frontend/src/app/components/login/components/login/login.component.ts
+++ frontend/src/app/components/login/components/login/login.component.ts
@@ -6,18 +6,30 @@ import {
 } from "@angular/core";
 import { Router } from "@angular/router";
 import { BsModalRef, BsModalService } from "ngx-bootstrap/modal";
-
+import { LoginService } from "src/app/services/login.service";
+import Swal from "sweetalert2";
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements AfterViewInit {
+  // * Fields
+  public emptyFieldAlert: boolean = false;
+  public emailAlert: boolean = false;
+  public password!: string;
+  public email!: string;
+  public disableSubmit: boolean = true;
+
   // * Modal
   public modalRef?: BsModalRef;
   @ViewChild("loginModal") loginModal!: TemplateRef<any>;
 
-  constructor(private _router: Router, private _modalService: BsModalService) {}
+  constructor(
+    private _router: Router,
+    private _modalService: BsModalService,
+    private _loginService: LoginService
+  ) {}
 
   ngAfterViewInit(): void {
     this.openModal(this.loginModal);
@@ -27,11 +39,55 @@ export class LoginComponent implements AfterViewInit {
     this.modalRef = this._modalService.show(loginModal);
   };
 
+  private _closeModal = (): void => {
+    this._modalService.hide();
+  };
+
   public navigateLogin = (): void => {
     this._router.navigate(["/login"]);
   };
 
+  verifEmail = (email: string): void => {
+    this.emailAlert = !this._loginService.isEmail(email);
+  };
+
   public onSubmit = (values: any): void => {
     console.log(values);
+
+    // * Reset Modal Fields
+    this.email = "";
+    this.password = "";
+    this.disableSubmit = true;
+
+    Swal.fire({
+      position: "top",
+      icon: "success",
+      title: "You are Succesfully Log In!",
+      showConfirmButton: false,
+      timer: 1125,
+    });
+
+    (() => {
+      setTimeout(() => {
+        this._router.navigate(["/dashboard"]);
+        this._closeModal();
+      }, 1000);
+    })();
+  };
+
+  public getValue = (f: any): void => {
+    this.emptyFieldAlert = !this.email || !this.password ? true : false;
+    this.disableSubmit =
+      this.email && this.password && this._loginService.isEmail(this.email)
+        ? false
+        : true;
+  };
+
+  public resetLoginModal = (): void => {
+    this.email = "";
+    this.password = "";
+    this.disableSubmit = true;
+    this.emptyFieldAlert = false;
+    this.emailAlert = false;
   };
 }
--- frontend/src/app/components/login/login.module.ts
+++ frontend/src/app/components/login/login.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ModalModule } from "ngx-bootstrap/modal";
 import { FormsModule } from "@angular/forms";
+import { SweetAlert2Module } from "@sweetalert2/ngx-sweetalert2";
 
 import { LoginComponent } from "./components/login/login.component";
 
+import { LoginService } from "src/app/services/login.service";
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, ModalModule.forRoot(), FormsModule],
-  providers: [],
+  imports: [
+    CommonModule,
+    ModalModule.forRoot(),
+    FormsModule,
+    SweetAlert2Module.forRoot(),
+  ],
+  providers: [LoginService],
   exports: [LoginComponent],
 })
 export class LoginModule {}
